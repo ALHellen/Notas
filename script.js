55c93a15532e05feb473f266884e612d950f1569
ALHellen <hellen.abade@outlook.com>
2019-09-24 18:58:53 -0300

    teste

---
+++ script.js
@@ -0,0 +1,24 @@
+// let mensagem = "Olá Mundo!"
+// let comando = prompt("Digite sua mensagem");
+// alert(comando);
+// alert(mensagem);
+// console.log(mensagem);
+
+// let numero1 = parseInt(prompt("Digite o 1º número: "));
+// let numero2 = parseInt(prompt("Digite o 2º número: "));
+// let soma = (numero1 + numero2);
+// alert(soma);
+
+// let dataNascimento = parseInt(prompt("Digite o ano do seu nascimento: "));
+// let calculoIdade = (2019 - dataNascimento);
+// let resultado = alert(calculoIdade);
+
+// let num1 = parseInt(prompt("Digite o 1º número: "));
+// let num2 = parseInt(prompt("Digite o 2º número: "));
+// let num3 = parseInt(prompt("Digite o 3º número: "));
+// let media = (num1 + num2 + num3)/3;
+// alert("A média entre os 3 números é: " + media);
+
+// let num1 = (prompt("Digite o 1º número: "));
+// let num2 = (prompt("Digite o 2º número: "));
+// alert(num1-num2);
